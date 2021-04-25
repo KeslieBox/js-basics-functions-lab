8d983a4cc8ac21316d48b11290508f815d6aca8d
KeslieBox <kesliebox@gmail.com>
2021-04-25 18:37:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceTravelledInFeet(num1, num2){
+   return num2 > num1 ? (num2-num1) * 264 : (num1-num2) * 264
+}
+
+function distanceFromHqInBlocks(num){
+   return (num > 42) ? (num - 42) : (42 - num)
+}
+
+function distanceFromHqInFeet(num){
+    return distanceFromHqInBlocks(num) * 264
+}
+
+function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
